4ed2a0c1b741c55b54ef890585b486ce8fff9b93
xkiio <99291539+xkiio@users.noreply.github.com>
2022-04-11 18:42:55 +1200

    print customer details
    added code to print customer details

---
+++ Components/order_print-v1.py
@@ -0,0 +1,13 @@
+
+customer_details = {"name": "nuno","phone": "1234567876","house": "66","street": "Eve St","suburb": "Howick","region": "Auckland","postcode": "2010"}
+
+# list to store ordered commissions
+order_list = ['Poster Style','Square Canvas/Album Cover']
+# list to store ordered commission prices
+order_cost = [130, 60]
+
+print("\n {} \n{} \n{} \n{} \n{}".format(customer_details['name'],customer_details['phone'],customer_details['house'],customer_details['street'],customer_details['suburb'],customer_details['region'],customer_details['postcode']))
+count = 0
+for item in order_cost:
+    print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
+    count = count  +1
--- Components/order_print-v2.py
+++ Components/order_print-v2.py
@@ -6,6 +6,7 @@ order_list = ['Poster Style','Square Canvas/Album Cover']
 # list to store ordered commission prices
 order_cost = [130, 60]
 
+print("\n Customer Name: {}\nCustomer Phone: {}\nCustomer House Number: {}\nCustomer Street Name: {}\nCustomer Suburb: {}\nCustomer Region: {}\nCustomer Postcode: {}".format(customer_details['name'],customer_details['phone'],customer_details['house'],customer_details['street'],customer_details['suburb'],customer_details['region'],customer_details['postcode']))
 count = 0
 for item in order_cost:
     print("Ordered: {} Cost ${:.2f}".format(item, order_cost[count]))
